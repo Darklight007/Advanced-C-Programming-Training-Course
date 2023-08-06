af2994e4a1b60bee7208e448a6c980559cfe9f29
Darklight007 <homayoun.jamshidi@gmail.com>
2023-08-06 15:07:18 -0700

    Add readme

---
+++ section6_37.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int main()
+{
+
+    deque<string> line;
+
+    line.push_back("Customer 1");
+    line.push_front("Customer 2");
+
+    line.push_back("Customer 3");
+
+
+for (int i=0;i<line.size();i++){
+
+    cout << line[i] << endl;
+}
+ line.pop_back();
+ line.pop_front();
+
+for (int i=0;i<line.size();i++){
+
+    cout << line[i] << endl;
+}
+
+
+    return 0;
+}
